db5f265208ec64612bcea95fa19f7df15a3ea29b
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2023-07-12 12:16:17 -0700

    updated timeout

--- vault/init.sh
+++ vault/init.sh
@@ -3,14 +3,14 @@
 echo 'INFO: Starting Raft cluster setup'
 
 # Configure cluster A
-kubectl exec vault-0 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-a-keys.json
-sleep 15s
+kubectl exec -ti vault-0 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-a-keys.json
+sleep 15
 
 echo 'INFO: vault-0 initialized'
 
 # Unseal vault-0
 export VAULT_UNSEAL_KEY_CLUSTER_A=$(jq -r ".unseal_keys_b64[]" cluster-a-keys.json)
-kubectl exec vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY_CLUSTER_A
+kubectl exec -ti vault-0 -- vault operator unseal $VAULT_UNSEAL_KEY_CLUSTER_A
 
 echo 'INFO: Unsealed vault-0'
 
@@ -23,14 +23,14 @@ kubectl exec -ti vault-2 -- vault operator unseal $VAULT_UNSEAL_KEY_CLUSTER_A
 echo 'INFO: Nodes added to cluster A'
 
 # Configure cluster B
-kubectl exec vault-3 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-b-keys.json
+kubectl exec -ti vault-3 -- vault operator init -key-shares=1 -key-threshold=1 -format=json > cluster-b-keys.json
 sleep 15
 
 echo 'INFO: vault-3 initialized'
 
 # Unseal vault-3
 VAULT_UNSEAL_KEY_CLUSTER_B=$(jq -r ".unseal_keys_b64[]" cluster-b-keys.json)
-kubectl exec vault-3 -- vault operator unseal $VAULT_UNSEAL_KEY_CLUSTER_B
+kubectl exec -ti vault-3 -- vault operator unseal $VAULT_UNSEAL_KEY_CLUSTER_B
 
 echo 'INFO: Unsealed vault-3'
 
