9fab8b6cada873c1a375c7ff523f7f695582d9fc
Jeremy Aranas <jeremy.aranas@hashicorp.com>
2023-07-14 11:17:51 -0700

    updated parameter

--- vault/k8s_auth.sh
+++ vault/k8s_auth.sh
@@ -72,7 +72,7 @@ KUBE_CA_CERT=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clust
 KUBE_HOST=$(kubectl config view --raw --minify --flatten --output='jsonpath={.clusters[].cluster.server}') 
 
 # Configure the k8s auth method to use the vault-auth service account JWT, location of the k8s host and its certificate
-kubectl exec -ti vault-0 -- vault write auth/kubernetes/config token_reviewer_jwt="$VAULT_TOKEN_REVIEW_JWT" kubernetes_host="$KUBE_HOST" kubernetes_ca_cert="$KUBE_CA_CERT" 
+kubectl exec -ti vault-0 -- vault write auth/kubernetes/config token_reviewer_jwt="$VAULT_TOKEN_REVIEW_JWT" kubernetes_host="$KUBE_HOST" kubernetes_ca_cert="$KUBE_CA_CERT" disable_local_ca_jwt="true" 
 
 # Read the k8s config
 kubectl exec -ti vault-0 -- vault read auth/kubernetes/config
